7587f948b2213db4143438dd54ea77e3beabda85
Rothsword <nathonrothsword@gmail.com>
2017-04-06 18:01:13 +0800

    new

--- src/hk/edu/polyu/comp3222/vfs/client/ClientOff.java
+++ src/hk/edu/polyu/comp3222/vfs/client/ClientOff.java
@@ -3,70 +3,33 @@ package hk.edu.polyu.comp3222.vfs.client;
 import hk.edu.polyu.comp3222.vfs.Executer;
 import hk.edu.polyu.comp3222.vfs.core.VirtualDisk;
 import hk.edu.polyu.comp3222.vfs.util.*;
+
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
  * Created by lidawei on 04/04/2017.
  */
-public class ClientOff extends Client{
-    public ClientOff(VirtualDisk disk){
+public class ClientOff extends Client {
+    public ClientOff(VirtualDisk disk) {
         super(disk);
     }
 
     @Override
-    public void action(){
-        while(true){
+    public void action() {
+        while (true) {
             String input = Executer.clientCmd.readLine();
             IOService output = null;
-            StringTokenizer st=new StringTokenizer(input," ");
-            String[] cmd = new String[10];
+            StringTokenizer st = new StringTokenizer(input, " ");
+            ArrayList<String> cmd = null;
             int count = 0;
-            while(st.hasMoreTokens()) {
-                cmd[count++] = st.nextToken();
+            while (st.hasMoreTokens()) {
+                cmd.add(st.nextToken());
             }
-            CommandType c = CommandType.valueOf(cmd[0]);
-            switch (c){
-                case COPY:
-                    CopyCmd copy = new CopyCmd();
-                    copy.command(cmd,output, disk);
-                    break;
-                case CD:
-                    NavigationCmd cd = new NavigationCmd();
-                    cd.command(cmd,output, disk);
-                    break;
-                case LIST:
-                    ListCmd ls = new ListCmd();
-                    ls.command(cmd, output,disk);
-                    break;
-                case MKDIR:
-                    MkdirCmd mk = new MkdirCmd();
-                    mk.command(cmd, output, disk);
-                    break;
-                case MOVE:
-                    MoveCmd mv = new MoveCmd();
-                    mv.command(cmd, output, disk);
-                    break;
-                case QUIT:
-                    QuitCmd q = new QuitCmd();
-                    q.command(cmd, output, disk);
-                    break;
-                case RENAME:
-                    RenameCmd r = new RenameCmd();
-                    r.command(cmd, output, disk);
-                    break;
-                case SEARCH:
-                    SearchCmd s = new SearchCmd();
-                    s.command(cmd, output, disk);
-                    break;
-                case HELP:
-                    HelpCmd h = new HelpCmd();
-                    h.command(cmd, output, disk);
-                    break;
-                case TOUCH:
-                    TouchCmd t = new TouchCmd();
-                    t.command(cmd, output, disk);
-                    break;
-            }
+            CommandType c = CommandType.valueOf(cmd.get(0));
+            CommandReciever receiver = new CommandReciever();
+            receiver.execute(c, cmd, output, disk);
+
         }
     }
 }
--- src/hk/edu/polyu/comp3222/vfs/util/CommandReciever.java
+++ src/hk/edu/polyu/comp3222/vfs/util/CommandReciever.java
@@ -1,8 +1,61 @@
 package hk.edu.polyu.comp3222.vfs.util;
 
+import hk.edu.polyu.comp3222.vfs.core.VirtualDisk;
+
+import java.util.ArrayList;
+
+import static hk.edu.polyu.comp3222.vfs.util.CommandType.*;
+
 /**
  * Created by lidawei on 05/04/2017.
  */
 public class CommandReciever {
+    public CommandReciever() {
+    }
 
+    public void execute(CommandType c, ArrayList<String> cmd, IOService output, VirtualDisk disk) {
+        switch (c) {
+            case COPY:
+                CopyCmd copy = new CopyCmd();
+                copy.command(cmd, output, disk);
+                break;
+            case CD:
+                NavigationCmd cd = new NavigationCmd();
+                cd.command(cmd, output, disk);
+                break;
+            case LIST:
+                ListCmd ls = new ListCmd();
+                ls.command(cmd, output, disk);
+                break;
+            case MKDIR:
+                MkdirCmd mk = new MkdirCmd();
+                mk.command(cmd, output, disk);
+                break;
+            case MOVE:
+                MoveCmd mv = new MoveCmd();
+                mv.command(cmd, output, disk);
+                break;
+            case QUIT:
+                QuitCmd q = new QuitCmd();
+                q.command(cmd, output, disk);
+                break;
+            case RENAME:
+                RenameCmd r = new RenameCmd();
+                r.command(cmd, output, disk);
+                break;
+            case SEARCH:
+                SearchCmd s = new SearchCmd();
+                s.command(cmd, output, disk);
+                break;
+            case HELP:
+                HelpCmd h = new HelpCmd();
+                h.command(cmd, output, disk);
+                break;
+            case TOUCH:
+                TouchCmd t = new TouchCmd();
+                t.command(cmd, output, disk);
+                break;
+        }
+
+    }
 }
